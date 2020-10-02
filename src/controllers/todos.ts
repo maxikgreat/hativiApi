c173090655e663382cba39a73e9f9b3dc1e3d8b0
maxikgreat <maximvasylenko228322@gmail.com>
2020-10-02 22:31:09 +0200

    Example architecture with todos

---
+++ src/controllers/todos.ts
@@ -0,0 +1,31 @@
+import { Response, Request } from "express"
+import { ITodo } from "../types/todo"
+import Todo from "../models/Todo"
+
+const getTodos = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const todos: ITodo[] = await Todo.find()
+    res.status(200).json({ todos })
+  } catch (error) {
+    throw error
+  }
+}
+
+
+const deleteTodo = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const deletedTodo: ITodo | null = await Todo.findByIdAndRemove(
+      req.params.id
+    )
+    const allTodos: ITodo[] = await Todo.find()
+    res.status(200).json({
+      message: "Todo deleted",
+      todo: deletedTodo,
+      todos: allTodos,
+    })
+  } catch (error) {
+    throw error
+  }
+}
+
+export { getTodos, deleteTodo }
