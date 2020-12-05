2662d5466daf4667f783bd01ea54fb84940a361f
Maksym Vasylenko <maximvasylenko228322@gmail.com>
2020-12-05 12:02:51 +0100

    Added template html to rend

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import fs from 'fs';
+import path from 'path';
 
 import auth0Routes from './routes/auth0.route';
 import instaRoutes from './routes/insta.route';
 
-// moved exec to package.json
-
-// import dotenv from 'dotenv';
-// const result = dotenv.config();
-// if (result.error) throw result.error;
-
+if (process.env.NODE_ENV === 'production') {
+  fs.copyFileSync( // copy html
+    path.join(__dirname, '../../index.html'),
+    path.join(__dirname, '../index.html'),
+  );
+}
 
 const port = process.env.PORT || 3001;
 const server = express();
@@ -21,6 +23,11 @@ server.use('/api/v1/insta', instaRoutes);
 
 (async () => {
   try {
+    
+    server.get('*', (_, res) => {
+      res.sendFile(path.join(__dirname, '../index.html'));
+    });
+    
     server.listen(port, (err?: any) => {
       if (err) throw err;
       console.log(`Ready on port ${port} - ${process.env.NODE_ENV}`)
